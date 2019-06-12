8dbfa8c49fcfc780b962a37e063b67420fb92d63
DCcheng <cdc87296750@gmail.com>
2019-06-12 15:15:42 +0800

    V0.1.8
    Signed-off-by: DCcheng <cdc87296750@gmail.com>

--- src/kernel/Kernel.php
+++ src/kernel/Kernel.php
@@ -8,7 +8,7 @@
  *
  * You must use 'Kernel::init()' in the project bootstrap to initialize this program
  * If you have not initialize this program , you will get a error message "Trying to get property of non-object"
- * 
+ *
  */
 
 
@@ -19,47 +19,88 @@ use Exception;
 
 class Kernel
 {
+    /**
+     * @var \Kernel\Container
+     */
     public $container;
 
+    /**
+     * @var null
+     */
     public static $app = null;
 
-    public function __construct(Container $container,$arr = [])
+    /**
+     * Kernel constructor.
+     * @param \Kernel\Container $container
+     * @param array $arr
+     */
+    public function __construct(Container $container, $arr = [])
     {
         $this->container = $container;
         $abstracts = $this->setClass();
-        foreach ($arr as $key=>$value){
+        foreach ($arr as $key => $value) {
             $abstracts[$key] = $value;
         }
-        foreach ($abstracts as $abstract => $concrete){
+        foreach ($abstracts as $abstract => $concrete) {
             $this->container->bind($abstract, $concrete);
             $this->$abstract = $this->container->make($abstract);
         }
     }
 
+    /**
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     * @throws Exception
+     */
     public function __call($name, $arguments)
     {
-        if(is_null(self::$app)){
-           throw new Exception("Please initialize the kernel program and you can use 'Kernel::init()' to initialize the program");
-        }
-        return $this->$name;
+        return self::callMethod($name, $arguments);
     }
 
+    /**
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     * @throws Exception
+     */
     public static function __callStatic($name, $arguments)
     {
-        if(is_null(self::$app)){
+        return self::callMethod($name, $arguments);
+    }
+
+    /**
+     * @param $name
+     * @param $arguments
+     * @return mixed
+     * @throws Exception
+     */
+    private static function callMethod($name, $arguments)
+    {
+        if (is_null(self::$app)) {
             throw new Exception("Please initialize the kernel program and you can use 'Kernel::init()' to initialize the program");
+        }
+        if (!isset(self::$app->$name) || is_null(self::$app->$name)) {
+            throw new Exception("Not found the 'Kernel::" . $name . "()' method , you may be check up the initialize array");
         }
         return self::$app->$name;
     }
 
+    /**
+     * @param array $arr
+     * @return Kernel|null
+     */
     public static function init($arr = [])
     {
-        if(is_null(self::$app)) {
+        if (is_null(self::$app)) {
             self::$app = new Kernel(new Container(), $arr);
         }
         return self::$app;
     }
 
+    /**
+     * @return array
+     */
     public function setClass()
     {
         return [
@@ -69,7 +110,7 @@ class Kernel
             "maps" => \Kernel\Maps\Maps::class,
             "serial" => \Kernel\Serial\Serial::class,
             "acg" => \Kernel\Acg\AcgLaravel5::class,
-            "qrcode"=>\Kernel\Qrcode\QRcode::class
+            "qrcode" => \Kernel\Qrcode\QRcode::class
         ];
     }
 }
