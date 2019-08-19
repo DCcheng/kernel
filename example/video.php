3cfd5d1f11bdb37ac887fa1014ba52f29f28ada2
DCcheng <cdc87296750@gmail.com>
2019-08-19 10:12:05 +0800

    V0.1.18master
    Signed-off-by: DCcheng <cdc87296750@gmail.com>

--- example/video.php
+++ example/video.php
@@ -11,11 +11,12 @@ require_once "../vendor/autoload.php";
 use Kernel\Kernel;
 
 $kernel = Kernel::init();
-
+echo Kernel::video()->screenCaptureCommand("E:/download/3.mkv","image","E:/download/",150,160);
+die();
 try {
     Kernel::command()->addCommand([
         Kernel::video()->mergeCommand(["E:/download/2.mkv","E:/download/5.mkv"],"E:/download/5.mp4"),
-        Kernel::video()->mergeCommand(["E:/download/2.mkv","E:/download/5.mkv"],"E:/download/5.mp4")
+        Kernel::video()->screenCaptureCommand("E:/download/3.mkv","image","E:/download/",150,160)
     ])->execute();
 } catch (Exception $exception) {
     echo $exception->getMessage();
--- src/kernel/Command/VideoCommand.php
+++ src/kernel/Command/VideoCommand.php
@@ -14,6 +14,7 @@ use Exception;
 class VideoCommand
 {
     use Commod;
+
     /**
      * @param $source
      * @param $target
@@ -29,9 +30,9 @@ class VideoCommand
         if(file_exists($target))
             throw new Exception("生成的目标文件已经存在，请重新输入文件名");
         $command = ["ffmpeg -safe 0 -i " . $source];
-        if ($start_time != null && (is_int($start_time) || preg_match("/^(([0-1]?\d)|(2[0-4])):[0-5]?\d:[0-5]?\d\s?(AM|PM)?$/", $start_time)))
+        if ($start_time != null && (is_int($start_time) || preg_match("/^(([0-1]?\d)|(2[0-4])):[0-5]?\d:[0-5]?\d$/", $start_time)))
             $command[] = " -ss " . $start_time;
-        if ($start_time != null && (is_int($start_time) || preg_match("/^(([0-1]?\d)|(2[0-4])):[0-5]?\d:[0-5]?\d\s?(AM|PM)?$/", $end_time)))
+        if ($start_time != null && (is_int($start_time) || preg_match("/^(([0-1]?\d)|(2[0-4])):[0-5]?\d:[0-5]?\d$/", $end_time)))
             $command[] = " -to " . $end_time;
         $command[] = $target == null?$source:$target;
         return implode(" ",$command);
@@ -71,5 +72,58 @@ class VideoCommand
             $command .= " && rm -Rf ".$filename;
         }
         return $command;
+    }
+
+    //视频截图
+
+    /**
+     * @param $source
+     * @param $target_name
+     * @param $path
+     * @param $start_time
+     * @param null $end_time
+     * @return string
+     * @throws Exception
+     */
+    public function screenCaptureCommand($source, $target_name,$path, $start_time,$end_time = null){
+
+        $command = ["ffmpeg"];
+
+        if ($start_time != null && (is_int($start_time) || preg_match("/^(([0-1]?\d)|(2[0-4])):[0-5]?\d:[0-5]?\d$/", $start_time))) {
+            $command[] = "-ss " . $start_time;
+        }else{
+            throw new Exception("输入截取开始时间格式错误");
+        }
+
+        if($end_time != null  && (is_int($end_time) || preg_match("/^(([0-1]?\d)|(2[0-4])):[0-5]?\d:[0-5]?\d$/", $end_time))) {
+            $command[] = "-to " . $end_time;
+        }else{
+            throw new Exception("输入截取结束时间格式错误");
+        }
+
+        if(!file_exists($source))
+            throw new Exception("无法读取源文件，请重新检查源文件是否存在");
+        $command[] = "-i " . $source." -r 1 -q:v 2 -f image2";
+
+        if(!is_dir($path))
+            throw new Exception("目标文件路径丢失或者权限不足");
+
+        if($end_time == null) {
+            if (file_exists($target_name.".jpg"))
+                throw new Exception("生成的目标文件已经存在，请重新输入文件名");
+            $command[] = "-y " . $target_name.".jpg";
+        }else{
+            $dh = opendir($path);
+            while (($file = readdir($dh)) !== false){
+                if(!is_dir($path."/".$file) && $file != "." && $file != ".."){
+                    list($filename,$postfix) = explode(".",$file);
+                    if(strpos($target_name,$filename)===false && $postfix == "jpg"){
+                        throw new Exception("生成的目标文件与已经存在的“".$file."”文件名所包含的前缀一直，请重新输入文件名");
+                    }
+                }
+            }
+            $command[] = "-y " . $target_name."-%d.jpg";
+        }
+        return implode(" ",$command);
     }
 }
