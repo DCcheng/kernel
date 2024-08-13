b55e1ec772f4660dfd842d46e6f186728570a8bc
DCcheng <cdc87296750@gmail.com>
2024-08-13 11:59:12 +0800

    更新
    Signed-off-by: DCcheng <cdc87296750@gmail.com>

--- src/kernel/Command/Command.php
+++ src/kernel/Command/Command.php
@@ -36,27 +36,12 @@ class Command implements CommandInterface
      * @param array $result
      * @return string
      */
-//    public function execute($bool = true,$binPath = "/usr/bin",&$result = array())
-//    {
-//        $command = "PATH=".$binPath."&".implode("&&",$this->command);
-//        if(!self::isWindowsSystem() && $bool)
-//            $command .= " > /dev/null &2>1&";
-//        $this->command = [];
-//        return exec($command,$result);
-//    }
-    public function execute($bool = true, $binPath = "/usr/bin", &$result = array())
+    public function execute($bool = true,$binPath = "/usr/bin",&$result = array())
     {
-        #linux 后台执行
-        if (!self::isWindowsSystem() && $bool) {
-            foreach ($this->command as $item) {
-                $command = "PATH=" . $binPath . "& nohup " . $item . " > /dev/null &2>1&";
-                exec($command, $result);
-            }
-            return 0;
-        } else {
-//            前台执行
-            $command = "PATH=" . $binPath . "&" . implode("&&", $this->command);
-            return exec($command, $result);
-        }
+        $command = "PATH=".$binPath."&".implode("&&",$this->command);
+        if(!self::isWindowsSystem() && $bool)
+            $command .= " > /dev/null &2>1&";
+        $this->command = [];
+        return exec($command,$result);
     }
 }
